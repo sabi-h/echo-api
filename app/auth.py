bd618cfac1827e17c7211f80c62af750c345f046
sabi-h <sabi-h@users.noreply.github.com>
2025-07-03 15:25:57 +0100

    fix

--- app/auth.py
+++ app/auth.py
@@ -7,7 +7,7 @@ from typing import Optional
 import os
 
 # Security configurations
-SECRET_KEY = os.getenv("SECRET_KEY", "wkvnuivhjsbjsbhjfnsiufvhewhfjsfnedhjshjbshjdkkdnfjewds")
+SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM", "HS256")
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", "2400"))
 
@@ -67,9 +67,9 @@ async def authenticate_user(username: str, password: str):
 def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     to_encode = data.copy()
     if expires_delta:
-        expire = datetime.utcnow() + expires_delta
+        expire = datetime.now() + expires_delta
     else:
-        expire = datetime.utcnow() + timedelta(minutes=15)
+        expire = datetime.now() + timedelta(minutes=15)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
